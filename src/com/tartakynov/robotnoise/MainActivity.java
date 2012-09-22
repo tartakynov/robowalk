a0eda85b2b423fb4270a4fe03d837c2444057b20
Artem Tartakynov <artem.tad@gmail.com>
2012-09-23 03:06:24 +0700

    flurry analytics
    --HG--
    branch : flurry

--- src/com/tartakynov/robotnoise/MainActivity.java
+++ src/com/tartakynov/robotnoise/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tartakynov.robotnoise;
 
+import com.flurry.android.FlurryAgent;
 import com.tartakynov.robotnoise.Preferences.OnPreferenceChangeListener;
 import com.tartakynov.robotnoise.VolumeCircleView.ICircleAngleChanged;
 
@@ -54,6 +55,20 @@ public class MainActivity extends Activity {
 	super.onDestroy();
 	doUnbindService();
     }	
+
+    @Override
+    protected void onStart() {
+	super.onStart();
+	if (getResources().getBoolean(R.bool.flurry_enabled)) {
+	    FlurryAgent.onStartSession(this, getResources().getString(R.string.flurry));	    
+	}
+    }
+
+    @Override
+    protected void onStop() {
+	super.onStop();
+	FlurryAgent.onEndSession(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
